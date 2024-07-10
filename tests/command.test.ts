ed9b8626137e5fca039a01d26c4798045e68d053
takoba <plane25@gmail.com>
2024-07-11 03:10:19 +0900

    :white_check_mark: Add testcase in tests/command.test.ts
    - testcase: it occurred error what exists dir already

--- tests/command.test.ts
+++ tests/command.test.ts
@@ -1,7 +1,20 @@
+import path from 'path'
 import { describe, it, expect } from '@jest/globals'
+
+const appName = 'test-app'
+const alreadyExistsPath = path.resolve('.', appName)
+jest.mock('fs', () => ({
+  existsSync: (path: string) => path === alreadyExistsPath,
+}))
 import Command from '../lib/command'
 
+const processExitMock = (process.exit = jest.fn((code) => code as never))
+
 describe('export const Command', () => {
+  afterEach(() => {
+    processExitMock.mockReset()
+  })
+
   it('write help text to stdout', () => {
     // arrange
     const stdoutWriterMock = (process.stdout.write = jest.fn())
@@ -13,5 +26,24 @@ describe('export const Command', () => {
     expect(stdoutWriterMock).toHaveBeenCalledWith(
       expect.stringContaining('create-typescript-app-base <app_name> [options]'),
     )
+  })
+  it('occurred error what exists dir already', () => {
+    // arrange
+    //const stderrWriterMock = (process.stderr.write = jest.fn())
+    const consoleErrorMock = (console.error = jest.fn())
+
+    // act
+    Command([appName])
+
+    // assert
+    expect(processExitMock).toHaveBeenCalled()
+    expect(processExitMock).toHaveBeenCalledWith(1)
+    expect(consoleErrorMock).toHaveBeenLastCalledWith(
+      expect.stringContaining(`ERROR: ${alreadyExistsPath} is exists. command is finish.`),
+    )
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
   })
 })
