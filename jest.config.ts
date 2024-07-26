f0fea35c0fb39d81c5651b8b8adfaae11b243bf4
takoba <plane25@gmail.com>
2024-07-26 20:26:41 +0900

    :wrench: Update jest.config.ts)
    - apply `JestConfigWithTsJest` type explicitly
    - add `moduleNameMapper` property
        - add @types/tiged.d.ts (in first, use `pathsToModuleNameMapper` function. but not support what using wildcard(`*`) in paths props on tsconfig.json)

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,12 @@
-import type { Config } from 'jest'
+import type { JestConfigWithTsJest } from 'ts-jest'
 
-const config: Config = {
+const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.{test,spec}.?(c|m)ts?(x)'],
+  moduleNameMapper: {
+    // @todo When have been supported type definition files in tiged, remove it
+    '^tiged$': '<rootDir>/@types/tiged.d.ts',
+  },
 }
 export default config
