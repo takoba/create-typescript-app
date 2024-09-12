a4281c404442f268d920b4504a7abb0b15c2a204
takoba <plane25@gmail.com>
2024-09-12 19:02:55 +0900

    :recycle: Remove unnecessary code & replace depended code on tiged

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,5 @@ const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/tests/**/*.{test,spec}.?(c|m)ts?(x)'],
-  moduleNameMapper: {
-    // @todo When have been supported type definition files in tiged, remove it
-    '^tiged$': '<rootDir>/@types/tiged.d.ts',
-  },
 }
 export default config
